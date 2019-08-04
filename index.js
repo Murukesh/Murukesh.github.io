968b2d1152bde10f47143126eec1fba51c2fa608
Murukesh Sadasivan <murukesh@gmail.com>
2019-08-04 11:30:57 +0530

    heroku

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function(req, res) {
-    res.write('Hello ', JSON.stringify(req.headers)); //write a response to the client
+    res.write('Hello ', req.url); //write a response to the client
     res.end(); //end the response
 }).listen(process.env.PORT); //the server object listens on port 8080
